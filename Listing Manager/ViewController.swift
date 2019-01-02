e6670bf9c379d2007909607d2fcf9fe701fc8ca8
John Baker <john@b4k3r.com>
2019-01-01 19:32:08 -0500

    Initial Commit

---
+++ Listing Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Listing Manager
+//
+//  Created by John Baker on 1/1/19.
+//  Copyright © 2019 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
